4384c12b55b2c68e5c5528660fe1e8c7c48985a0
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-09-08 16:57:25 +0300

    watch utility

--- bin/watch.js
+++ bin/watch.js
@@ -2,18 +2,31 @@
 
 "use strict";
 
-var chokidar = require('chokidar'),
+var chokidar = require("chokidar"),
     shell = require("shelljs"),
     args = process.argv.slice(2).length > 0 ? process.argv.slice(2) : [null]
 
-var watcher = chokidar.watch('.', {
+var debug = ~args.indexOf("--debug") || ~args.indexOf("-d")
+var watch = getParameter(args, "--watch")[0] || getParameter(args, "-w")[0] || "."
+var help = ~args.indexOf("--help") || ~args.indexOf("-h")
+args = remove(args, ["--debug", "-d", "--watch", watch])
+//console.log("options", debug, watch, help)
+
+var watcher = chokidar.watch(watch, {
   ignored: /[\/\\]\./,
   persistent: true
 })
 
-var debug = ~args.indexOf("--debug") ||
-            ~args.indexOf("-d") ?
-              (args = remove(args, ["--debug", "-d"]), true) : false
+if(help) {
+  console.log(
+    "usage: gd-watch [options] command [command command *]" +
+    "\r\n\r\nOPTIONS:" +
+    "\r\n\t-d | --debug\tverbose" +
+    "\r\n\t-w | --watch\t\twatch glob pattern" +
+    "\r\n\t-h | --help\tprint help"
+  )
+  process.exit(0)
+}
 
 if(debug)
   console.log("Running in debug mode")
@@ -21,16 +34,16 @@ if(debug)
 watcher
   .on("change", function(file) {
     if(debug)
-      console.log('File', file, 'has been changed')
+      console.log("File", file, "has been changed")
     triggerCommands(args.map(function(cmd) {
       if(debug)
-        console.log("Running command", cmd)
+        console.log("Running command '%s'", cmd)
       return cmd// + " " + file
     }))
   })
   .on("ready", function() {
     if(debug)
-      console.log("Watching...")
+      console.log("Watching...", watch)
   })
   .on("error", function(error) {
     if(debug)
@@ -41,6 +54,7 @@ function remove(list, keys) {
   if(!Array.isArray(keys))
     keys = [keys]
   return list.filter(function(item) {
+    //console.log(item, keys, !~keys.indexOf(item))
     return !~keys.indexOf(item)
   })
 }
@@ -49,4 +63,26 @@ function triggerCommands(commands) {
   commands.forEach(function(cmd) {
     shell.exec(cmd)
   })
+}
+
+function getParameter(paramenters, get) {
+  var ret = [];
+
+  paramenters.forEach(function(n, i, all) {
+    if(n === get && all[i + 1])
+      ret.push(all[i + 1])
+  })
+  return ret;
+}
+
+function fst(arr) { return arr.length > 0 ? arr[0] : [null] }
+
+function format(str) {
+  var args = [].splice.call(arguments, 1)
+  //console.log("%d of args", args.length)
+  args.forEach(function(substitute) {
+    str = str.replace(/%s|%d/, substitute)
+    //console.log("replacing with %s: %s", substitute, str)
+  })
+  return str
 }
